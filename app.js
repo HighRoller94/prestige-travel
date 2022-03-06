7cd4a4cad21399c15605e38af05f0504b72120fa
Ash Bridges <ashbridges@virginmedia.com>
2022-03-06 23:26:53 +0000

    new design

--- app.js
+++ app.js
@@ -14,15 +14,65 @@ const scrollToTop = () => {
 }
 window.addEventListener('scroll', scrollTop);
 
+// Menu highlights
+
+const highlightCurrent = () => {
+    
+    const highlight = document.querySelector('.menu-highlight')
+
+    const homeMenu = document.querySelector('#home-page')
+    const destinationsMenu = document.querySelector('#destinations-page')
+    const exploreMenu = document.querySelector('#explore-page')
+    const discoverMenu = document.querySelector('#discover-page')
+
+    let scrollPosition = window.scrollY
+
+    if (window.innerWidth < 960 && scrollPosition < 786) {
+        homeMenu.classList.add('menu-highlight')
+        destinationsMenu.classList.remove('menu-highlight')
+        exploreMenu.classList.remove('menu-highlight')
+        discoverMenu.classList.remove('menu-highlight')
+        return
+    } else if (window.innerWidth < 960 && scrollPosition < 2000) {
+        homeMenu.classList.remove('menu-highlight')
+        destinationsMenu.classList.add('menu-highlight')
+        exploreMenu.classList.remove('menu-highlight')
+        discoverMenu.classList.remove('menu-highlight')
+        return
+    } else if (window.innerWidth < 960 && scrollPosition < 3128) {
+        homeMenu.classList.remove('menu-highlight')
+        destinationsMenu.classList.remove('menu-highlight')
+        exploreMenu.classList.add('menu-highlight')
+        discoverMenu.classList.remove('menu-highlight')
+        return
+    } else if (window.innerWidth < 960 && scrollPosition < 4000) {
+        homeMenu.classList.remove('menu-highlight')
+        destinationsMenu.classList.remove('menu-highlight')
+        exploreMenu.classList.remove('menu-highlight')
+        discoverMenu.classList.add('menu-highlight')
+    }
+
+    if ((highlight && window.innerWidth < 960 && scrollPosition < 600) || highlight) {
+        highlight.classList.remove('menu-highlight')
+    }
+}
+
+window.addEventListener('scroll', highlightCurrent);
+window.addEventListener('click', highlightCurrent);
 
 // Mobile Menu
 
-const menu = document.querySelector('#mobile-menu')
+const menu = document.querySelector('#open-menu')
+const menuClose = document.querySelector('#close-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 const mobileMenu = () => {
-    menu.classList.toggle('is-active')
+    menu.classList.add('active')
     menuLinks.classList.toggle('active')
+}
+
+const closeMenu = () => {
+    menuLinks.classList.remove('active')
 }
 
 const links = document.querySelectorAll(".navbar__links");
@@ -30,11 +80,12 @@ const links = document.querySelectorAll(".navbar__links");
 links.forEach(link => {
     link.addEventListener("click", () => {
         menuLinks.classList.remove('active');
-        menu.classList.toggle('active');
+        menu.classList.remove('active');
     })
 })
 
 menu.addEventListener('click', mobileMenu);
+menuClose.addEventListener('click', closeMenu);
 
 // Change nav on scroll 
 
